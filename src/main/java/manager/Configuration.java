f7f58d987c73e466945fb2dbb3a9870ceb17292e
TayaHatum <21taya21@gmail.com>
2022-03-21 21:10:13 +0200

    added tets

---
+++ src/main/java/manager/Configuration.java
@@ -0,0 +1,48 @@
+package manager;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+//"deviceName": "Nex",
+//        "platformName": "Android",
+//        "platformVersion": "8.0",
+//        "appPackage": "com.example.svetlana.scheduler",
+//        "appActivity": ".presentation.splashScreen.SplashScreenActivity"
+public class Configuration {
+    protected static AppiumDriver<MobileElement>driver;
+
+
+    @BeforeSuite
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("deviceName","Nex");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
+        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
+
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("app","/Users/tayahatum/Tools/apk/v.0.0.3.apk");
+
+        driver= new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+
+
+
+
+    }
+
+
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/tetssuper/TestsTestsTests.java
@@ -0,0 +1,12 @@
+package tetssuper;
+
+import manager.Configuration;
+import org.testng.annotations.Test;
+
+public class TestsTestsTests extends Configuration {
+
+    @Test
+    public void ytetstete(){
+
+    }
+}
